9b803bd735d74cdd0e0b596f91b712563e1d1304
Luca <balasoiu2005@yahoo.com>
2026-03-05 22:04:40 +0200

    adaugat contor igienizari

--- frontend/constants/ApiConfig.ts
+++ frontend/constants/ApiConfig.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://146.190.224.202:8080/api';
+export const API_BASE_URL = 'http://192.168.100.34:8080/api';
 ///need to change this to the new api
 ///server: 146.190.224.202

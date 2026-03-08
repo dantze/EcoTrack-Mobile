10512a4309cdb75c14c96e60c1f24a908a1bc07e
Andrei Dan <adan@tudelft.nl>
2026-03-08 22:37:52 +0100

    version change

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -2,7 +2,7 @@ module.exports = {
     expo: {
         name: "EcoTrack",
         slug: "frontend",
-        version: "1.0.0",
+        version: "1.0.1",
         orientation: "portrait",
         icon: "./assets/images/icon.png",
         scheme: "frontend",

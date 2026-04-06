099c600b3bfc2c1980b3c7099a76a72a7d73cb1f
Andrei Dan <adan@tudelft.nl>
2026-04-06 09:30:31 +0200

    new version

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -2,7 +2,7 @@ module.exports = {
     expo: {
         name: "EcoTrack",
         slug: "frontend",
-        version: "1.1.0",
+        version: "1.2.0",
         orientation: "portrait",
         icon: "./assets/images/icon.png",
         scheme: "frontend",
@@ -19,7 +19,7 @@ module.exports = {
         },
         android: {
             package: "com.damiprod.ecotrack",
-            versionCode: 3,
+            versionCode: 4,
             adaptiveIcon: {
                 foregroundImage: "./assets/images/adaptive-icon.png",
                 backgroundColor: "#ffffff"
